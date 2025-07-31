22b5fb67d84b3cedc37c9476c58441d58617aa3b
Ahmed Alshughri <alshughriahmed@gmail.com>
2025-07-31 14:33:30 +0000

    Initial commit of new project

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+import React from 'react';
+
+const ChatPage = () => {
+  return (
+    // This is the full UI code we created earlier
+    // ... (The full chat page JSX code)
+  );
+};
+export default ChatPage;
