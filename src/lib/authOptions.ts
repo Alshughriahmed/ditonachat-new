07ffcf0b85994decf36804dfd983b8ad3233cc28
Ahmed Alshughri <alshughriahmed@gmail.com>
2025-08-07 22:44:11 +0000

    Add custom sign-in page with Google only

--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -25,21 +25,6 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID!,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
-    }),
-    EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST!,
-        port: Number(process.env.EMAIL_SERVER_PORT!),
-        auth: {
-          user: process.env.EMAIL_SERVER_USER!,
-          pass: process.env.EMAIL_SERVER_PASSWORD!,
-        },
-      },
-      from: process.env.EMAIL_FROM!,
-    }),
   ],
   session: { strategy: 'jwt' },
   callbacks: {
